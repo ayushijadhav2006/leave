3f1a87876b354c79a7a0f987f796cc515d447937
ayushijadhav2006 <ayushijadhav2006@gmail.com>
2024-06-26 13:47:24 +0530

    welcome page changes

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,7 @@ import 'primereact/resources/primereact.min.css';
 
 
 export const Welcome = () => {
-    const header = <h1 style={styles.headerText}>✨ Welcome to WeLLeave ✨</h1>;
+    const header = <h1 style={styles.headerText}>✨ Welcome to WeL-ELSP ✨</h1>;
 
 
 
@@ -14,7 +14,7 @@ export const Welcome = () => {
             <Card style={styles.card} className="p-shadow-8">
                 {header}
                 <div style={styles.imageContainer}>
-                    WeLLeave is a Leave Prediction Simulator, An attempt to automate the process of predicting leaves involving Leave Request , Forecasting and Count Prediction. 
+                    WeL-ELSP is a Leave Prediction Simulator, An attempt to automate the process of predicting leaves involving Leave Request , Forecasting and Count Prediction. 
                 </div>
             </Card>
         </div>
